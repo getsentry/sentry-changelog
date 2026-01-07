ce1b7ee129978dfcd852d182f359bd0b1b6dacc8
Charly Gomez <charly.gomez@sentry.io>
2026-01-07 13:39:08 +0100

    fix image fill

--- src/client/components/article.tsx
+++ src/client/components/article.tsx
@@ -25,11 +25,14 @@ export function Article({
   return (
     <article className={`bg-white rounded-lg shadow-lg mb-8 ${className}`}>
       {image && (
-        <Image
-          className="object-cover relative w-full h-64 rounded-lg rounded-b-none"
-          src={image}
-          alt={title}
-        />
+        <div className="relative w-full h-64">
+          <Image
+            className="object-cover rounded-lg rounded-b-none"
+            src={image}
+            alt={title}
+            fill
+          />
+        </div>
       )}
       <div className="p-6">
         <h3 className="text-3xl text-primary font-semibold mb-2">{title}</h3>
